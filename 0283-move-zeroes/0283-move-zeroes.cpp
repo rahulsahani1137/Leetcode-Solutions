bd772f8e42d4a9b30ad2225c6f7a10b92641d353
Rahul Sahani <66432121+rahulsahani1137@users.noreply.github.com>
2025-07-30 23:57:49 +0530

    Time: 0 ms (100%), Space: 23.8 MB (80.77%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,21 +1,48 @@
 // Brute Force Approach
-// TC: O(n) & SC: O(n)
+// TC: O(N) & SC: O(N)
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//         vector<int> tempArray;
+//         for (int i=0; i<nums.size(); i++) {
+//             if(nums[i] != 0) {
+//                 tempArray.push_back(nums[i]);
+//             }
+//         }
+
+//         int numberOfZeroes = nums.size() - tempArray.size();
+
+//         for(int j=0; j<numberOfZeroes; j++) {
+//             tempArray.push_back(0);
+//         }
+
+//         nums = tempArray;
+//     }
+// };
+
+// Optimal Approach (Using 2 Pointers)
+// TC: O(N) & SC: O(1)
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        vector<int> tempArray;
-        for (int i=0; i<nums.size(); i++) {
-            if(nums[i] != 0) {
-                tempArray.push_back(nums[i]);
+        int j = -1;
+        int n = nums.size();
+        
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == 0) {
+                j = i;
+                break;
             }
         }
 
-        int numberOfZeroes = nums.size() - tempArray.size();
+        // No non zero numbers
+        if(j == -1) return;
 
-        for(int j=0; j<numberOfZeroes; j++) {
-            tempArray.push_back(0);
+        for(int i = j+1; i < n; i++) {
+            if(nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
+            }
         }
-
-        nums = tempArray;
     }
 };
